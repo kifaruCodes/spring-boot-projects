1b1c59925ee5c2a12df87f5178a247a147f9cea8
Kifaru Codes <kifarutech@gmail.com>
2023-06-10 11:18:53 +0300

    an update on the sprongboot backend api. I have an issue with the database section, so I have resorted to refining my knowledge on postgres

--- back-end-api/src/main/java/com/example/backendapi/student/StudentController.java
+++ back-end-api/src/main/java/com/example/backendapi/student/StudentController.java
@@ -1,27 +1,24 @@
 package com.example.backendapi.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
-    @GetMapping()
-    public List<Student> hello(){
+    private final StudentService studentService;
 
-        return List.of(
-                new Student(
-                        1L,
-                        "Kifaru",
-                        "kifaru@mail.com",
-                        LocalDate.of(2000, Month.MARCH, 3),
-                        24
-                )
-        );
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping()
+    public List<Student> getStudents(){
+        return studentService.getStudents();
     }
 }
